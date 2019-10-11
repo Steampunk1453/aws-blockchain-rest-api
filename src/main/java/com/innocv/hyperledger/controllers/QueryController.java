1acd91452cb9e745061bb0929ecf9be9524af161
Diego Escalada <diego.escalada@innocv.com>
2019-10-11 21:16:56 +0200

    create donors endpoint

---
+++ src/main/java/com/innocv/hyperledger/controllers/QueryController.java
@@ -0,0 +1,31 @@
+package com.innocv.hyperledger.controllers;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.hyperledger.fabric.sdk.HFClient;
+import org.hyperledger.fabric.sdk.NetworkConfig;
+import org.hyperledger.fabric.sdk.helper.Config;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@Slf4j
+@RestController
+public class QueryController {
+
+    @SneakyThrows
+    @GetMapping("/donors")
+    private void list() {
+        log.debug("Query all donors");
+
+        File configFile = new File("/home/ec2-user/aws-blockchain-rest-api/src/main/resources/config-profile.yml");
+        NetworkConfig networkConfig = NetworkConfig.fromYamlFile(configFile);
+        HFClient client = HFClient.createNewInstance();
+        client.loadChannelFromConfig("mychannel", networkConfig);
+
+        log.debug("Created client");
+    }
+
+}
