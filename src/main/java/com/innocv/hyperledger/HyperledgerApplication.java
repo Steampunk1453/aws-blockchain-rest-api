e58900b4aedacdb5c3fc4438266e4c752a815892
Diego Escalada <diego.escalada@innocv.com>
2019-10-11 20:06:48 +0200

    initial project creation

---
+++ src/main/java/com/innocv/hyperledger/HyperledgerApplication.java
@@ -0,0 +1,13 @@
+package com.innocv.hyperledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperledgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperledgerApplication.class, args);
+    }
+
+}
